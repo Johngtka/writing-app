b5e4552f2a74e97e2858c9d90d54dc7a6802110d
Johngtka <johngtka@gmail.com>
2022-12-10 21:36:37 +0100

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.first = "Hello World!"
+    $scope.desc = "Write Something"
+    $scope.text = "Your text is writing here :-)"
+})
+function conv() {
+    const hide = document.getElementById("a");
+    hide.classList.add("printhide");
+    const hide1 = document.getElementById("text")
+    hide1.classList.add("panel")
+}
