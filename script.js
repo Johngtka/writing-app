8ec7c6b12bd27b99c451ffebfbfc435287476800
Johngtka <johngtka@gmail.com>
2022-12-11 18:16:40 +0100

    .

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var app = angular.module("myApp", []);
+
 app.controller("myCtrl", function ($scope) {
-    $scope.first = "Hello World!"
+    $scope.first = "Text to PDF Saver!"
     $scope.desc = "Write Something"
     $scope.text = "Your text is writing here :-)"
+    $scope.info = 'To contact with us, write an email to:';
+    $scope.mail = '<a href="#">example@gmail.com</a>'
+    function concat() {
+        var element = document.getElementById("info")
+        element.innerHTML = $scope.info + $scope.mail;
+    }
+    concat();
 })
 const hide = document.getElementById("a")
 function conv() {
